120e6e16dc257d58a5fb36f3db58befb72a52a23
Leon Wang <wangjq10@gmail.com>
2017-12-05 03:06:39 +0000

    Part 1

--- hello.py
+++ hello.py
@@ -1,13 +1,33 @@
 import os
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello():
+    #for Cloud9 only, this is for step tracing.
+    #import pdb; pdb.set_trace()
+    
     return "Hello World!"
+
+@app.route("/")
+def index():
+    #return "Index Page"
+    return url_for('show_user_profile', username='Leon')
+
+
+@app.route("/username/<username>")
+def show_user_profile(username):
+    return "Hello {}".format(username)
+    
+    
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    return "Post {}".format(post_id)
+    
     
 if __name__ == "__main__":
     host = os.getenv('IP', '0.0.0.0')
-    port = os.getenv('PORT', 5000)
+    port = int(os.getenv('PORT', 5000))
+    app.debug = True
     app.run(host=host, port=port)
