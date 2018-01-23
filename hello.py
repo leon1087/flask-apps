81c64f353a86669eafbe6a6b72dac5cf326ecb5d
Leon Wang <wangjq10@gmail.com>
2018-01-23 21:55:11 +0000

    Adding error  Logging

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 import os
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask, request, render_template, redirect, url_for, flash, session
 
 app = Flask(__name__)
@@ -18,7 +20,8 @@ def login():
             return redirect(url_for('welcome'))
         else:
             error = "Incorrect username and password."
-        
+            app.logger.warning("Incorrect username and password for user (%s).", request.form['username'])
+            
     return render_template('login.html', error = error)
     
 @app.route('/logout')
@@ -38,4 +41,8 @@ if __name__ == "__main__":
     port = int(os.getenv('PORT', 5000))
     app.debug = True
     app.secret_key = '\xad\xa5N\xaf\x8e\x19\x9cn}\x03\xb6[\xbbE\xbb\x1aYf\xd5\x96\x14\x9f\xe1\x81'
+    #logging
+    handler = RotatingFileHandler("error.log", maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
     app.run(host=host, port=port)
